24403d68419b9bc36596eef1a090730153d69735
Ashim Sharma <ashimsharma337@gmail.com>
2023-02-02 13:01:00 -0500

    userService interface create to create user

---
+++ EcomApi/src/main/java/com/example/EcomApi/UserService/UserService.java
@@ -0,0 +1,11 @@
+package com.example.EcomApi.UserService;
+
+import com.example.EcomApi.Entities.User;
+
+public interface UserService {
+      
+	//create user
+	User createUser(User user);
+	
+	
+}
