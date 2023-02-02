860ee8b9071aaa0ec63f5919a6be0e7cd1f447d6
Ashim Sharma <ashimsharma337@gmail.com>
2023-02-01 23:29:19 -0500

    name added for a user table as a users

--- EcomApi/src/main/java/com/example/EcomApi/Entities/User.java
+++ EcomApi/src/main/java/com/example/EcomApi/Entities/User.java
@@ -1,9 +1,12 @@
 package com.example.EcomApi.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Table
+@Table(name="users")
 @Entity
 public class User {
 
