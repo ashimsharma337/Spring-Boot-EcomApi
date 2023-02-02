4cec2691e2c5bee0fa310cdfc66e41c9a02c8384
Ashim Sharma <ashimsharma337@gmail.com>
2023-02-02 14:16:21 -0500

    methods added on user service for crud operations

--- EcomApi/src/main/java/com/example/EcomApi/UserService/UserService.java
+++ EcomApi/src/main/java/com/example/EcomApi/UserService/UserService.java
@@ -1,11 +1,21 @@
 package com.example.EcomApi.UserService;
 
+import java.util.List;
+
 import com.example.EcomApi.Payloads.UserDto;
 
 public interface UserService {
       
 	//create user
 	UserDto createUser(UserDto user);
+	//update user
+	UserDto updateUser(UserDto user, Integer userId);
+	//get user by id
+	UserDto getUserById(Integer userId);
+	//get all users
+	List<UserDto> getAllusers();
+	//delete user
+	void deleteUser(Integer userId);
 	
 	
 }
