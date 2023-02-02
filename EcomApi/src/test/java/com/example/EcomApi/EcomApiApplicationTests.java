f09ec6779f5dfcad747d7113ab8918401fb6c021
Ashim Sharma <ashimsharma337@gmail.com>
2023-02-01 20:00:35 -0500

    first push database connection completed on properties file

---
+++ EcomApi/src/test/java/com/example/EcomApi/EcomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcomApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
