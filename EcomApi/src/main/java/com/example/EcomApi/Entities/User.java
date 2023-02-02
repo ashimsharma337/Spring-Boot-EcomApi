74dc2775469f2f7ff9886f1e819c93a1b5766dfc
Ashim Sharma <ashimsharma337@gmail.com>
2023-02-01 20:32:38 -0500

    User Entity created with getter and setter

---
+++ EcomApi/src/main/java/com/example/EcomApi/Entities/User.java
@@ -0,0 +1,63 @@
+package com.example.EcomApi.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
+public class User {
+
+	//Default constructor
+	public User(){
+		
+	}
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	private String address;
+	
+	//getters
+	  public Long getId(){
+	     return this.id;
+	  }
+
+	  public String getName(){
+	     return this.name;
+	  }
+	  
+	  public String getEmail(){
+		     return this.email;
+	  }
+	  
+	  public String getPassword(){
+		     return this.password;
+	  }
+	  
+	  public String getAddress(){
+		     return this.address;
+	  }
+	  
+	//setters
+	  public void setId(Long id){
+	     this.id = id;
+	  }
+
+	 public void setName(String name){
+	    this.name = name;
+	 }
+	 
+	 public void setEmail(String email){
+		    this.email = email;
+     }
+	 
+	 public void setPassword(String password){
+		    this.password = password;
+	 }
+	 
+	 public void setAddress(String address){
+		    this.address = address;
+	 }
+	
+}
